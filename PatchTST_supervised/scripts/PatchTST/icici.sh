b075246a9431b860ab72a2f94d566b83cd1740ee
VirenLoka <virenloka17@gmail.com>
2026-05-12 19:21:13 +0530

    first_commit

--- PatchTST_supervised/scripts/PatchTST/icici.sh
+++ PatchTST_supervised/scripts/PatchTST/icici.sh
@@ -5,40 +5,41 @@ fi
 if [ ! -d "./logs/LongForecasting" ]; then
     mkdir ./logs/LongForecasting
 fi
-seq_len=336
+seq_len=60
 model_name=PatchTST
 
 root_path_name=./dataset/
-data_path_name=weather.csv
-model_id_name=weather
-data_name=custom
+data_path_name=icici_3-years.csv
+model_id_name=icici
+data_name=icici
 
 random_seed=2021
-for pred_len in 96 192 336 720
+for pred_len in 5 10 20 30
 do
     python3 -u run_longExp.py \
       --random_seed $random_seed \
       --is_training 1 \
       --root_path $root_path_name \
       --data_path $data_path_name \
-      --model_id $model_id_name_$seq_len'_'$pred_len \
+      --model_id $model_id_name'_'$seq_len'_'$pred_len \
       --model $model_name \
       --data $data_name \
-      --features M \
+      --features MS \
+      --target 'Close Price' \
       --seq_len $seq_len \
       --pred_len $pred_len \
-      --enc_in 21 \
-      --e_layers 3 \
-      --n_heads 16 \
-      --d_model 128 \
-      --d_ff 256 \
+      --enc_in 12 \
+      --e_layers 2 \
+      --n_heads 4 \
+      --d_model 64 \
+      --d_ff 128 \
       --dropout 0.2\
       --fc_dropout 0.2\
       --head_dropout 0\
-      --patch_len 16\
-      --stride 8\
+      --patch_len 8\
+      --stride 4\
       --des 'Exp' \
       --train_epochs 100\
       --patience 20\
-      --itr 1 --batch_size 128 --learning_rate 0.0001 >logs/LongForecasting/$model_name'_'$model_id_name'_'$seq_len'_'$pred_len.log 
+      --itr 1 --batch_size 16 --learning_rate 0.0001 >logs/LongForecasting/$model_name'_'$model_id_name'_'$seq_len'_'$pred_len.log
 done
